dd74b31315460d2fe7c3adb8b9c140758d2fd2b1
Buu342 <buu342@hotmail.com>
2025-04-01 04:09:46 +0100

    April fools

--- home.js
+++ home.js
@@ -4,6 +4,10 @@ function main()
     var icon = document.getElementsByClassName("me-myself-and-i")[0].childNodes[1];
     switch (date.getMonth())
     {
+        case 3:
+            if (date.getDate() == 1)
+                icon.src = "memyselfi_aprilfools.png";
+            break;
         case 9:
             icon.src = "memyselfi_october.png";
             break;
